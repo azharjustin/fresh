081a39a538977995168ee0fbdf575ff07cdef55a
azharjustin <azharjustin37@gmail.com>
2022-02-22 22:56:44 -0800

    new 1

---
+++ mov/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  const datas=[ {
+    name:"azhar" ,
+  profile:"https://a10.gaanacdn.com/gn_img/artists/a7LWBzWzXA/7LWBLgNKzX/size_xl_1631801811.webp"
+  },
+  {
+    name:"azhar" ,
+  profile:"https://a10.gaanacdn.com/gn_img/artists/a7LWBzWzXA/7LWBLgNKzX/size_xl_1631801811.webp"
+  },
+  {
+    name:"azhar" ,
+  profile:"https://a10.gaanacdn.com/gn_img/artists/a7LWBzWzXA/7LWBLgNKzX/size_xl_1631801811.webp"
+  },
+  ];
+  return (
+    <div className="App">
+     {datas.map((dt)=> <Welcome  name={dt.name}profile={dt.profile}/>)}
+      
+    </div>
+  );
+}
+function Welcome({name,profile}){
+  // const name="azhar";
+  return(
+    <div className="welcome">
+     <img className="profile" src={profile} alt="pic"/>
+      <h1 >hello {name}</h1>
+    </div>
+  );
+}
+export default App;
